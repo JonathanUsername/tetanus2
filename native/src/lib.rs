4f9159d56a9043e9cca23bef58936ce6d6f3d810
jonathanusername <jonathan.j.king@gmail.com>
2021-12-05 22:17:44 +0000

    Add single threaded rust

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -4,6 +4,15 @@ fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
 }
 
+fn triangle_single(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let ceil = cx.argument::<JsNumber>(0)?.value();
+
+    let sum: u128 = (0..ceil as u128).sum::<u128>();
+    Ok(cx.number(sum as f64))
+}
+
 register_module!(mut cx, {
-    cx.export_function("hello", hello)
+    cx.export_function("hello", hello)?;
+    cx.export_function("triangle_single", triangle_single)?;
+    Ok(())
 });
