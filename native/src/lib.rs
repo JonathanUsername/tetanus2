dd6316a4e38b8dcdb3e6b7bcc2f3d5e8f1af262b
jonathanusername <jonathan.j.king@gmail.com>
2021-12-05 22:22:38 +0000

    actually_parallel

--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,4 +1,5 @@
 use neon::prelude::*;
+use rayon::prelude::*;
 
 fn hello(mut cx: FunctionContext) -> JsResult<JsString> {
     Ok(cx.string("hello node"))
@@ -11,8 +12,16 @@ fn triangle_single(mut cx: FunctionContext) -> JsResult<JsNumber> {
     Ok(cx.number(sum as f64))
 }
 
+fn triangle_parallel(mut cx: FunctionContext) -> JsResult<JsNumber> {
+    let ceil = cx.argument::<JsNumber>(0)?.value();
+
+    let sum: u128 = (0..ceil as u128).into_par_iter().sum::<u128>();
+    Ok(cx.number(sum as f64))
+}
+
 register_module!(mut cx, {
     cx.export_function("hello", hello)?;
     cx.export_function("triangle_single", triangle_single)?;
+    cx.export_function("triangle_parallel", triangle_parallel)?;
     Ok(())
 });
